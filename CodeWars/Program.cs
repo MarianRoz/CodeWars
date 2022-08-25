ae439d51eadb6374fcd3ff04d3cba2dd3ce91b55
Marian <rozumnyi.marian@gmail.com>
2022-08-25 12:40:20 +0300

    Add all folders

---
+++ CodeWars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CodeWars.Tasks;
+
+namespace BubblesortOnce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in BubbleSortOnce.BubbleSortOnceResolve(new int[] { 10, 7, 5, 3, 1, 2, 4, 6, 8, 9 }))
+            {
+                Console.Write($"{item} ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+//CodeWars
---
+++ CodeWars/Tasks/BubbleSortOnce.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CodeWars.Tasks
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/56b97b776ffcea598a0006f2/train/csharp
+    /// </summary>
+    public static class BubbleSortOnce
+    {
+        public static int[] BubbleSortOnceResolve(int[] input)
+        {
+
+            int[] newArray = new int[input.Length];
+            Array.Copy(input, newArray, input.Length);
+            // Code the Bubblesort Algorithm here :D
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int e = 0; e < input.Length - 1; e++)
+                {
+                    if (input[e] > input[e + 1])
+                    {
+                        var temp = input[e];
+                        input[e] = input[e + 1];
+                        input[e + 1] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+    }
+}
