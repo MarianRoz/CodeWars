7cc33645a2788ceed6ad8fa26aa698d6d0b36fb0
Marian <rozumnyi.marian@gmail.com>
2022-09-27 15:09:55 +0300

    Added two tasks

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -7,6 +7,7 @@ namespace CodeWars.Tasks
     {
         static void Main(string[] args)
         {
+
             foreach (var item in Tasks.BubbleSortOnceResolve(new int[] { 10, 7, 5, 3, 1, 2, 4, 6, 8, 9 }))
             {
                 Console.Write($"{item} ");
@@ -18,6 +19,13 @@ namespace CodeWars.Tasks
             Console.ReadLine();
 
 
+            int a = 2, r = 4, n = 5;
+            Console.WriteLine($"Result: {GeometricSequenceElements(a, r, n)}");
+
+
+            int start = 1; int finish = 6;
+            Cats(start, finish);
+            
 
         }
     }
