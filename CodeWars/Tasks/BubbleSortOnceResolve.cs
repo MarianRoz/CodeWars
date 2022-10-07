cd7568d23fff1a60381c79a4a00e3fc9d79dd7d3
Marian <rozumnyi.marian@gmail.com>
2022-10-07 10:30:51 +0300

    Add new file for solution Bubble Sort Once Resolve

--- CodeWars/Tasks/BubbleSortOnceResolve.cs
+++ CodeWars/Tasks/BubbleSortOnceResolve.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 namespace CodeWars.Tasks
 {
-    public class BubbleSortOnceResolve
+    /// <summary>
+    /// https://www.codewars.com/kata/56b97b776ffcea598a0006f2/train/csharp
+    /// </summary>
+    public static partial class Tasks
     {
-        public BubbleSortOnceResolve()
+        public static int[] BubbleSortOnceResolve(int[] input)
         {
+            int[] newArray = new int[input.Length];
+            Array.Copy(input, newArray, input.Length);
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] > input[i + 1])
+                {
+                    var temp = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = temp;
+                }
+            }
+            return input;
         }
     }
 }
